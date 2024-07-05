adf0f5df106b1a78a45dd5b92306fba5efc2b3e9
Olefishyfisher <59668763+Olefishyfisher@users.noreply.github.com>
2024-07-05 19:06:50 -0400

    Update albums.js

--- albums.js
+++ albums.js
@@ -1,1 +1,16 @@
 
+const albums = [
+    {
+        rank: 99,
+        title: "Hotel California",
+        artist: "Eagles",
+        image: "hotel-california.jpg"
+    },
+    {
+        rank: 98,
+        title: "Another Album",
+        artist: "Another Artist",
+        image: "another-album.jpg"
+    },
+    // Add more albums here
+];
